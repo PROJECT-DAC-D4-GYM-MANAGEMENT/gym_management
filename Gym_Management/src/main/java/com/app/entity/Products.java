041c78966a1a5e053a2c5ff531b09365ffe8fa14
D4_80483_harshhargaonkar <82280185+D4-80483-harshhargaonkar@users.noreply.github.com>
2024-02-20 09:12:09 +0530

    products entity updated

--- Gym_Management/src/main/java/com/app/entity/Products.java
+++ Gym_Management/src/main/java/com/app/entity/Products.java
@@ -28,9 +28,15 @@
 package com.app.entity;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.persistence.CollectionTable;
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -43,12 +49,22 @@ import lombok.ToString;
 @Setter
 @ToString(callSuper = true)
 public class Products extends BaseEntity {
-    @Column(length = 20)
-    private String prodName;
-    @Column(length = 200)
-    private String description;
-    private double price;
-    private int stock;
-    private String supplier;
+	
+	@Column(length = 20)
+	private String name;
+	private double price;
+	private int stock;
+	private String supplier;
+	private String category;
+	
+	@ElementCollection(fetch = FetchType.EAGER)
+	@CollectionTable(name = "plan_images", joinColumns = @JoinColumn(name = "Product_id"))
+	@Column(name = "images")
+	private List<String> images = new ArrayList<>();
+
+	@ElementCollection(fetch = FetchType.EAGER)
+	@CollectionTable(name = "product_description", joinColumns = @JoinColumn(name = "Product_id"))
+	@Column(name = "description")
+	private List<String> description = new ArrayList<>();
 
 }
